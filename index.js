92f503321ab782d302a7b23d8302bfb1390e8177
Walki17 <walkii_@hotmail.com>
2024-10-19 15:38:17 -0300

    a

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-// Configuración para que el carrusel cambie cada 2 segundos
-$('#textCarousel').carousel({
-    interval: 2000
-});
+const texts = [
+    "...vestibulum mollis, tortor ac congue commodo",
+    "...lorem ipsum dolor sit amet",
+    "...phasellus eleifend semper tempor"
+];
+
+function changeText() {
+    const randomText = texts[Math.floor(Math.random() * texts.length)];
+    document.getElementById('random-text').innerText = randomText;
+}
+
+setInterval(changeText, 2000);
